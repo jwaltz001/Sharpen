7a0469bc6d14a40a13b9864c052e6c0982521dea
Justin Waltz <justinwaltz@Justins-iMac.local>
2019-09-18 10:03:04 -0500

    link for trivia challenge view works

---
+++ Models/TriviaChallenge.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Sharpen.Models
+{
+    public class TriviaChallenge
+    {
+        public int Id { get; set; }
+        public string Answer { get; set; }
+        public string Question { get; set; }
+        public int Value { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
