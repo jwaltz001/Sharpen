dca87d76e1504095bdb7521a1f6e1038808ba99e
Justin Waltz <justinwaltz@Justins-iMac.local>
2019-09-18 13:07:57 -0500

    got json response from jservice.io

--- Controllers/TriviaChallengeController.cs
+++ Controllers/TriviaChallengeController.cs
@@ -3,17 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Sharpen.Models;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Sharpen.Controllers
 {
+
     public class TriviaChallengeController : Controller
     {
+        //static HttpClient client = new HttpClient();
+        //static async Task RunAsync()
+        //{
+        //    // Update port # in the following line.
+        //    client.BaseAddress = new Uri("http://jservice.io/api/");
+        //    client.DefaultRequestHeaders.Accept.Clear();
+        //    client.DefaultRequestHeaders.Accept.Add(
+        //        new MediaTypeWithQualityHeaderValue("application/json"));
+
+        //    try
+        //    {
+        //        TriviaChallenge triviaChallenge = new TriviaChallenge;
+        //        // Get the question object
+        //        triviaChallenge = await GetProductAsync("random");
+        //        //ShowProduct(product);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine(e.Message);
+        //    }
+        //}
+
+        //static async IAsyncEnumerable<TriviaChallenge> GetProductAsync()
+        //{
+        //    client.BaseAddress = new Uri("http://jservice.io/api/");
+        //    client.DefaultRequestHeaders.Accept.Clear();
+        //    client.DefaultRequestHeaders.Accept.Add(
+        //        new MediaTypeWithQualityHeaderValue("application/json"));
+
+            
+            //TriviaChallenge triviaChallenge = null;
+            //HttpResponseMessage response = await client.GetAsync("random");
+            //if (response.IsSuccessStatusCode)
+            //{
+                //List<TriviaChallenge> respArr = await response.Content.ReadAsAsync<List<TriviaChallenge>>();
+                //yield return respArr[0];
+            //}
+            
+        //}
+
         // GET: /<controller>/
-        public IActionResult Index()
+
+        public async Task<TriviaChallenge> Index()
         {
-            return View();
+            Console.WriteLine("Hello");
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("http://jservice.io/api/random");
+            List<TriviaChallenge> respArr = await response.Content.ReadAsAsync<List<TriviaChallenge>>();
+            TriviaChallenge triviaChallenge = respArr[0];
+            //ViewData["Question"] = triviaChallenge.question;
+            //ViewData["Category"] = triviaChallenge.category.title;
+            return triviaChallenge;
         }
     }
 }
--- Models/TriviaChallenge.cs
+++ Models/TriviaChallenge.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 namespace Sharpen.Models
 {
+    public class Category
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+        public int clues_count { get; set; }
+    }
+
     public class TriviaChallenge
     {
-        public int Id { get; set; }
-        public string Answer { get; set; }
-        public string Question { get; set; }
-        public int Value { get; set; }
-        public int CategoryId { get; set; }
+        public int id { get; set; }
+        public string answer { get; set; }
+        public string question { get; set; }
+        public int value { get; set; }
+        public DateTime airdate { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+        public int category_id { get; set; }
+        public object game_id { get; set; }
+        public object invalid_count { get; set; }
+        public Category category { get; set; }
     }
+
+    //public class TriviaChallenge
+    //{
+    //    public int Id { get; set; }
+    //    public string Answer { get; set; }
+    //    public string Question { get; set; }
+    //    public int Value { get; set; }
+    //    public int CategoryId { get; set; }
+    //}
 }
